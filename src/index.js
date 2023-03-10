5de32800c4e7222e3d12d14f95766c804b06a7e4
Prasoon <prasoonmaurya813@gmail.com>
2023-03-11 03:25:36 +0530

    Todolist with nodeJS and mongoDB version: 2

--- src/index.js
+++ src/index.js
@@ -6,75 +6,258 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/todoListDB");
 
-let task
-let index
-let tasks = [{ title: "Welcome Chief!", description: "Add Tasks in todo list" }];
-let priorities = [];
-let dailys = [];
-let weekends = [];
-let monthlys = [];
+const Schema = new mongoose.Schema({
+  title: String,
+  description: String,
+});
+const Home = mongoose.model("Home", Schema);
+const Priority = mongoose.model("Priority", Schema);
+const Daily = mongoose.model("Daily", Schema);
+const Weekend = mongoose.model("Weekend", Schema);
+const Month = mongoose.model("Month", Schema);
 
+const task = new Home({
+  title: "Welcome Chief!",
+  description: "Add Tasks in todo list",
+});
+let defaultTasks = [task];
 
-const changeList =(reqBody, listArray)=>{
-  if (reqBody.btn == "ADD") {
-    task = {
-      title: reqBody.title,
-      description: reqBody.description
+// const changeList =(reqBody, listArray)=>{
+//   if (reqBody.btn == "ADD") {
+//     task = {
+//       title: reqBody.title,
+//       description: reqBody.description
+//     }
+//     listArray.push(task);
+//   }
+//   else {
+//     index = reqBody.btn;
+//     listArray.splice(index, 1);
+//   }
+// }
+
+app.get("/", async (req, res) => {
+  try {
+    const data = await Home.find();
+    if (data.length === 0) {
+      Home.insertMany(defaultTasks)
+        .then(() => {
+          res.redirect("/");
+        })
+        .catch((err) => {
+          console.error("Error saving user:", err.message);
+        });
+    } else {
+      res.render("index", { tasks: data, action: "/", listType: "Home" });
     }
-    listArray.push(task);
+  } catch (error) {
+    res.status(500).send(error.message); // Handle errors
   }
-  else {
-    index = reqBody.btn;
-    listArray.splice(index, 1);
+});
+app.post("/", async (req, res) => {
+  if (req.body.btn === "ADD") {
+    try {
+      const task = new Home(req.body);
+      // const homeItem = new Home({
+      //   title: req.body.title,
+      //   description: req.body.description,
+      // });
+      await task.save();
+    } catch (error) {
+      res.status(400).send(error.message); // Handle validation errors or other errors
+    }
+  } else {
+    try {
+      const deletingID = req.body.btn;
+      const t = await Home.findByIdAndRemove(deletingID);
+      // console.log(t);
+    } catch (error) {
+      res.status(500).send(error.message); // Handle validation errors or other errors
+    }
   }
-}
-
-app.get("/", (req, res) => {
-  res.render("index", { tasks: tasks, action: "/", listType: "Home" })
-});
-app.post("/", (req, res) => {
-  changeList(req.body, tasks);
-  res.redirect("/")
+  res.redirect("/");
+  // changeList(req.body, tasks);
 });
 
-app.get("/priority", (req, res) => {
-  res.render("priority", { tasks: priorities, action: "/priority", listType: "Priority List" })
+app.get("/priority", async (req, res) => {
+  try {
+    const data = await Priority.find();
+    if (data.length === 0) {
+      Priority.insertMany(defaultTasks)
+        .then(() => {
+          res.redirect("/priority");
+        })
+        .catch((err) => {
+          console.error("Error saving user:", err.message);
+        });
+    } else {
+      res.render("priority", {
+        tasks: data,
+        action: "/priority",
+        listType: "Priority List",
+      });
+    }
+  } catch (error) {
+    res.status(500).send(error.message); // Handle errors
+  }
 });
-app.post("/priority", (req, res) => {
-  changeList(req.body, priorities);
-  res.redirect("/priority")
+app.post("/priority", async (req, res) => {
+  if (req.body.btn === "ADD") {
+    try {
+      const task = new Priority(req.body);
+      await task.save();
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  } else {
+    try {
+      const deletingID = req.body.btn;
+      await Priority.findByIdAndRemove(deletingID);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+  res.redirect("/priority");
 });
 
-app.get("/daily", (req, res) => {
-  res.render("daily", { tasks: dailys, action: "/daily", listType: "Daily List" })
+app.get("/daily", async (req, res) => {
+  try {
+    const data = await Daily.find();
+    if (data.length === 0) {
+      Daily.insertMany(defaultTasks)
+        .then(() => {
+          res.redirect("/daily");
+        })
+        .catch((err) => {
+          console.error("Error saving user:", err.message);
+        });
+    } else {
+      res.render("daily", {
+        tasks: data,
+        action: "/daily",
+        listType: "Daily List",
+      });
+    }
+  } catch (error) {
+    res.status(500).send(error.message); // Handle errors
+  }
 });
-app.post("/daily", (req, res) => {
-  changeList(req.body, dailys);
-  res.redirect("/daily")
+app.post("/daily", async (req, res) => {
+  if (req.body.btn === "ADD") {
+    try {
+      const task = new Daily(req.body);
+      await task.save();
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  } else {
+    try {
+      const deletingID = req.body.btn;
+      await Daily.findByIdAndRemove(deletingID);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+  res.redirect("/daily");
 });
 
-app.get("/weekend", (req, res) => {
-  res.render("weekend", { tasks: weekends, action: "/weekend", listType: "Weekend List" })
+app.get("/weekend", async (req, res) => {
+  try {
+    const data = await Weekend.find();
+    if (data.length === 0) {
+      Weekend.insertMany(defaultTasks)
+        .then(() => {
+          res.redirect("/weekend");
+        })
+        .catch((err) => {
+          console.error("Error saving user:", err.message);
+        });
+    } else {
+      res.render("weekend", {
+        tasks: data,
+        action: "/weekend",
+        listType: "Weekend List",
+      });
+    }
+  } catch (error) {
+    res.status(500).send(error.message); // Handle errors
+  }
 });
-app.post("/weekend", (req, res) => {
-  changeList(req.body, weekends);
-  res.redirect("/weekend")
+app.post("/weekend", async (req, res) => {
+  if (req.body.btn === "ADD") {
+    try {
+      const task = new Weekend(req.body);
+      await task.save();
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  } else {
+    try {
+      const deletingID = req.body.btn;
+      await Weekend.findByIdAndRemove(deletingID);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+  res.redirect("/weekend");
 });
 
-app.get("/monthly", (req, res) => {
-  res.render("monthly", { tasks: monthlys, action: "/monthly", listType: "Monthly List" })
+app.get("/month", async (req, res) => {
+  try {
+    const data = await Month.find();
+    if (data.length === 0) {
+      Month.insertMany(defaultTasks)
+        .then(() => {
+          res.redirect("/month");
+        })
+        .catch((err) => {
+          console.error("Error saving user:", err.message);
+        });
+    } else {
+      res.render("month", {
+        tasks: data,
+        action: "/month",
+        listType: "Monthly List",
+      });
+    }
+  } catch (error) {
+    res.status(500).send(error.message); // Handle errors
+  }
 });
-app.post("/monthly", (req, res) => {
-  changeList(req.body, monthlys);
-  res.redirect("/monthly")
+app.post("/month", async (req, res) => {
+  if (req.body.btn === "ADD") {
+    try {
+      const task = new Month(req.body);
+      await task.save();
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  } else {
+    try {
+      const deletingID = req.body.btn;
+      await Month.findByIdAndRemove(deletingID);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+  res.redirect("/month");
 });
-
-
-
 
 app.listen(1830, function () {
   console.log("Hi DSS❤");
 });
+
+// git init 
+// git clone <<ssh code>>
+// git status
+// git add .
+// git commit -m "commit message"
+// git status
+
+// git remote add origin <<ssh code>>
+// git push -u origin master
