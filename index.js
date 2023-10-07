03c070b8d937ad1f83fc796ae72249d1453a3b61
Prasoon <prasoonmaurya813@gmail.com>
2023-10-07 15:42:09 +0530

    Replace index.js from src to main folder (So there is no need to define path for views folder explicitly ----> v2.3.0

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 
 const path = require("path");
-app.use(express.static(path.join(__dirname, "../public")));
+// app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "./public")));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+// app.set('views', path.join(__dirname, '../views'));
 
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/todoListDB");
